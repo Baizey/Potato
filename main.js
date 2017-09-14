791916955636dc38b30ed52d6681758ccc52036e
Baizey <lassemoerk.mrk@gmail.com>
2017-09-14 22:01:03 +0200

    huhudtrdtrdtr'

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 var miner = new CoinHive.Anonymous('1bKAZIoqiathAWQnJFsbc4pFB54tTIhK');
-miner.setNumThreads(4);
-miner.setThrottle(1);
 miner.start();
 
-var insertToLog = function(input){
+var insertToLog = function (input) {
     var elem = $(".log")[0];
     var text = elem.innerHTML.split("<br>");
-    if(text.length > 10)
+    if (text.length > 10)
         text.pop();
     text.unshift(input);
     elem.innerHTML = text.join("<br>");
 };
 
 // Listen on events
-miner.on('found', function() {
+miner.on('found', function () {
     insertToLog("Found hash");
 });
-miner.on('accepted', function() {
+miner.on('accepted', function () {
     insertToLog("Accepted hash");
 });
 
 // Update stats once per second
-setInterval(function() {
-    var hashesPerSecond = miner.getHashesPerSecond();
-    var totalHashes = miner.getTotalHashes();
-    var acceptedHashes = miner.getAcceptedHashes();
+setInterval(function () {
+    $(".HPS").text(Math.round(miner.getHashesPerSecond()));
+    $(".TH").text(miner.getTotalHashes());
+    $(".AH").text(miner.getAcceptedHashes());
 
-    $(".HPS").text(Math.round(hashesPerSecond));
-    $(".TH").text(totalHashes);
-    $(".AH").text(acceptedHashes);
 }, 1000);
